5eea6180261976e111035e55d1d86f38de0736d3
Dhavan Vaidya <quark@codingquark.com>
2016-07-30 17:07:51 +0530

    Added square root example

---
+++ examples/sqrt-itr-1.scm
@@ -0,0 +1,20 @@
+(define (improve guess x)
+  (averate guess (/ x guess)))
+
+(define (averate x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(define (square x)
+  (* x x))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
