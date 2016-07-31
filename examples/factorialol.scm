c4154ff9f101b38a6a08ea28d70492e3d777c55f
Dhavan Vaidya <quark@codingquark.com>
2016-07-31 13:04:44 +0530

    Added linear iteration

--- examples/factorialol.scm
+++ examples/factorialol.scm
@@ -3,3 +3,18 @@
   (if (= n 1)
       1
       (* n (factorial (- n 1)))))
+
+
+;; This is supposed to be non-recursive
+(define (factorial-itr n)
+  (fact-itr 1 1 n))
+
+;; WAT! this is recursion again!
+;; No, just because the procedure is calling
+;; itself doesn't mean it's recursive.
+(define (fact-itr product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-itr (* counter product)
+                (+ counter 1)
+                max-count)))
