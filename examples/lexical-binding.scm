ff58dbe4edfd551ae3b040acbb7066b9489cd6ed
Dhavan Vaidya <quark@codingquark.com>
2016-07-31 12:40:24 +0530

    Example of lexical binding

---
+++ examples/lexical-binding.scm
@@ -0,0 +1,17 @@
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (square x)
+  (* x x))
+
+;; See how variable x is accessed
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
