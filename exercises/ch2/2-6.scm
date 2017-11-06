42dce9a848d0c80eab72b8179b0fb53d6f87bc51
Dhavan Vaidya <quark@codingquark.com>
2017-11-06 11:09:30 +0530

    Ex 2-6: Tests

--- exercises/ch2/2-6.scm
+++ exercises/ch2/2-6.scm
@@ -16,5 +16,16 @@
 
 ;; https://en.wikipedia.org/wiki/Church_encoding#Calculation_with_Church_numerals
 (define (+ a b) (lambda (f) (lambda (x) ((a f) ((b f) x)))))
-;; Not sure if the following is correct:
+
+;; The following is incorrect.
+;; @TODO: debug
 ;; (define (+ m n) (lambda (m) (lambda (n) (lambda (f) (lambda (x) (m (f (n f x))))))))
+
+(define inc add1)
+(test 0 ((zero inc) 0))
+(test 1 (((add-1 zero) inc) 0))
+(test 2 (((add-1 (add-1 zero)) inc) 0))
+
+(test 1 ((one inc) 0))
+(test 2 ((two inc) 0))
+(test 4 (((+ two two) inc) 0))
