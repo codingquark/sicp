c1c5b4a88a98e0644697c28df5124bf5f244c10d
Dhavan Vaidya <quark@codingquark.com>
2017-11-15 12:37:48 +0530

    Ex 2-27: Added one more test that fails

--- exercises/ch2/2-27.scm
+++ exercises/ch2/2-27.scm
@@ -11,8 +11,7 @@
   (define (inner items acc)
     (cond ((null? items) acc)
           ((pair? (car items)) (inner (cdr items)
-                                      (cons (reverse (car items))
-                                            acc)))
+                                      (cons (deep-reverse (car items)) acc)))
           (else (inner (cdr items)
                        (cons (car items) acc)))))
   (inner items '()))
@@ -24,3 +23,5 @@
 (test (list (list 4 3) (list 2 1)) (deep-reverse x))
 (define x (list (list 1 2) 3))
 (test (list 3 (list 2 1)) (deep-reverse x))
+(define x (list 1 (list 2 (list 3))))
+(test (list 3 (list 2 (list 1))) (deep-reverse x)) ;; this fails!
