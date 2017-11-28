57765056c54bf89dde2868127ed6acdceb961ef4
Dhavan Vaidya <quark@codingquark.com>
2017-11-28 16:30:58 +0530

    Ex 2.35

--- exercises/ch2/2-34.scm
+++ exercises/ch2/2-34.scm
@@ -13,3 +13,4 @@
               coefficient-dequence))
 
 (test 79 (horner-eval 2 (list 1 3 0 5 0 1)))
+
---
+++ exercises/ch2/2-35.scm
@@ -0,0 +1,21 @@
+(use test)
+
+;; From examples:
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+(define (enumerate-tree tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+(define (count-leaves t)
+  (accumulate +
+              0
+              (map (lambda (x) 1)
+                   (enumerate-tree t))))
+
+(test 4 (count-leaves (cons (list 1 2) (list 3 4))))
